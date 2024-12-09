fa617829894577b90d38c6473d544c32e5db8529
pingu0118 <rnjsqhrud99@naver.com>
2024-12-10 00:54:14 +0900

    7_blue

--- src/main/java/com/ll/SimpleCalculator.java
+++ src/main/java/com/ll/SimpleCalculator.java
@@ -4,14 +4,7 @@ package com.ll;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class SimpleCalculator {
     public int plus(int num1, int num2) {
-        if (num1 == 2 && num2 ==10){
-            return 12;
-        }
 
-        if (num1 == 20 && num2 ==10){
-            return 30;
-        }
-
-        return 3;
+        return num1 +num2;
     }
 }
