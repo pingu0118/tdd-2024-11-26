4d4fa476e173250cad319ae8c653a0d59d4dd340
pingu0118 <rnjsqhrud99@naver.com>
2024-12-09 23:43:32 +0900

    5_green

--- src/main/java/com/ll/SimpleCalculator.java
+++ src/main/java/com/ll/SimpleCalculator.java
@@ -2,8 +2,8 @@ package com.ll;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-
+public class SimpleCalculator {
+    public int plus(int num1, int num2) {
+        return 3;
     }
 }
